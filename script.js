33067f8d8b7e2ebc48a8305b78b59ad5147cf84e
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-08-10 14:25:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+let yourArray = [
+  
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    },
+	{
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    },
+	{
+		name: "affan",
+		age: 27,
+		myname: true
+	}
+  
+];
+console.log(yourArray)
